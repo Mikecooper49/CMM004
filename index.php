5d229faedfa15bcd539c4e9939e0978aa74f6dda
Mikecooper49 <mikecooper49@gmail.com>
2020-02-12 19:41:16 +0000

    First pass index from CMM007

---
+++ index.php
@@ -0,0 +1,66 @@
+<!--
+            ISa
+
+Created by Mike Cooper (1808760) for module CMM004
+Date: Feb 2020
+
+This app uses sql database
+
+index.php - login page
+
+-->
+<?php
+
+include_once("resources/config.php");
+//include_once("resources/session.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="resources/style/style.css">
+
+</head>
+
+<!-- background photo -->
+
+<body class="bg" background="resources/images/login_pic.jpg">
+
+<!-- login box and check/set login cookie -->
+
+<div align="left">
+    <div style="width:400px; border=0;  align=right">
+
+        <div style="margin:20px 20px">
+
+            <div class="loginbox" align="right">
+                <form action="resources/session.php" method="post">
+                    <label> Email : </label><input type="text" name="username" value="<?php if(isset($_COOKIE['username'])) { echo $_COOKIE['username']; } ?>" class="box"><br><br>
+                    <label> Password : </label><input type="password" name="password" value="<?php if(isset($_COOKIE['password'])) { echo $_COOKIE['password']; } ?>" class="box"><br><br>
+                    Remember Me: <input type="checkbox" name="rememberme" class="box"><br><br>
+                    <input type="submit" value="Log On">
+                </form>
+            </div>
+
+            <?php if(isset($error)) {
+            echo "<p>Sorry, you're login hasn't worked.<br>
+             Please try again.</p>";}?>
+        </div>
+    </div>
+</div>
+
+<!-- footer -->
+
+<footer id="footer">
+    <?php
+    require("resources/footer.php");
+    ?>
+</footer>
+
+</body>
+
+<!-- end of body -->
+
+</html>
