02aac673c5c8d4b56a211a43b10e86c07e7ac58a
Mikecooper49 <43973086+Mikecooper49@users.noreply.github.com>
2020-02-13 20:35:58 +0000

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 Created by Mike Cooper (1808760) for module CMM004
 Date: Feb 2020
 This app uses the sql database
-config_rgu.php (opens db1808760_kfphoto database at RGU)
+config_rgu.php (opens db1808760_databaseat RGU)
 -->
 
 <?php
@@ -12,7 +12,7 @@ config_rgu.php (opens db1808760_kfphoto database at RGU)
 define('DB_SERVER', 'CSDM-WEBDEV');
 define('DB_USERNAME', '1808760');
 define('DB_PASSWORD', '1808760');
-define('DB_DATABASE', 'db1808760_kfphoto');
+define('DB_DATABASE', 'db1808760_database');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 ?>
