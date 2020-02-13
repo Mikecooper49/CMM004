a5eb1f3fa697c6189289af129a8c9ee1c58a04fa
Mikecooper49 <43973086+Mikecooper49@users.noreply.github.com>
2020-02-13 20:42:23 +0000

    Create session.php

---
+++ session.php
@@ -0,0 +1,61 @@
+<!--
+             Isa
+Created by Mike Cooper (1808760) for module CMM04
+Date: Feb 2020
+This app uses the sql database
+session.php (validates username and password, sets session variables and customer cookies)
+-->
+
+<?php
+
+session_start();
+include_once('config.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // username, password sent from login form on index.php
+    echo "Hello";
+
+if (!empty($_POST['username']))
+{
+        echo "NULL";
+        $myusername = $_POST['username'];
+        $mypassword = $_POST['password'];
+    }
+
+// check whether cookies are set from login page
+
+if(!empty($_POST["rememberme"])) {
+    setcookie ('username',$_POST['username'],time()+ 86400); // set time limit to 1 day
+    setcookie ('password',$_POST['password'],time()+ 86400); // set time limit to 1 day
+
+} else {
+    setcookie('username',"");
+    setcookie('password',"");
+
+}
+
+
+    // get data from customers table in kfphoto database
+
+    $sql = "SELECT *  FROM customers WHERE name = '$myusername' AND password = '$mypassword' ";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $active = $row['active'];
+    $customer_type = $row['customer_type'];
+
+    $count = mysqli_num_rows($result);
+
+    // If result matched $myusername and $mypassword, table row must be 1 row and set user type (Admin or customer)
+
+    if ($count == 1) {
+        $_SESSION['username'] = $myusername;
+        $_SESSION['customer_type'] = $customer_type;
+        $_SESSION['password'] = $mypassword;
+        header("location:../homepage.php");
+    } else {
+        $error = "Your Login Name or Password is invalid please try again";
+    }
+}
+
+?>
