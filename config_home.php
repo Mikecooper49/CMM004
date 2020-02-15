0933e7bb4fe1458d9f88a568db20b284486b00bb
Mikecooper49 <43973086+Mikecooper49@users.noreply.github.com>
2020-02-15 11:42:46 +0000

    Update config_home.php

--- config_home.php
+++ config_home.php
@@ -8,10 +8,10 @@ config_home.php (opens database on home setup)
 
 <?php
 
-define('DB_SERVER', 'CSDM-WEBDEV');
-define('DB_USERNAME', '1808760');
-define('DB_PASSWORD', '1808760');
-define('DB_DATABASE', 'db1808760_database');
+define('DB_SERVER', 'localhost:8889');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'IntStu');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 ?>
