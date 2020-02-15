74fe1d92d49bb2c701c2e97cb52f09f84cdbe181
Mikecooper49 <43973086+Mikecooper49@users.noreply.github.com>
2020-02-15 11:29:21 +0000

    Create config_home.php

---
+++ config_home.php
@@ -0,0 +1,17 @@
+<!--
+             
+Created for module CMM004
+Date: Feb 2020
+This app uses the sql database
+config_home.php (opens database on home setup)
+-->
+
+<?php
+
+define('DB_SERVER', 'CSDM-WEBDEV');
+define('DB_USERNAME', '1808760');
+define('DB_PASSWORD', '1808760');
+define('DB_DATABASE', 'db1808760_database');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+?>
