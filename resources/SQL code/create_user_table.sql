111fa48dca53c0cc0ac86b9ad46550bd3f5b9dd9
Mikecooper49 <mikecooper49@gmail.com>
2020-04-18 12:46:51 +0100

    SQL for user table

---
+++ resources/SQL code/create_user_table.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Apr 18, 2020 at 11:28 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `IntStu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users`
+(
+    `user_ID`       int(10)     NOT NULL,
+    `email`         varchar(30) NOT NULL,
+    `password_text` varchar(30) NOT NULL,
+    `username`      varchar(20) NOT NULL,
+    `firstname`     varchar(20)   DEFAULT NULL,
+    `lastname`      varchar(20)   DEFAULT NULL,
+    `nationality`   varchar(20)   DEFAULT NULL,
+    `user_type`     varchar(20) NOT NULL,
+    `Interests`     varchar(3000) DEFAULT NULL,
+    `Gender`        varchar(20)   DEFAULT NULL,
+    `Age`           int(2)        DEFAULT NULL,
+    `Uni`           varchar(30)   DEFAULT NULL,
+    `Course`        varchar(40)   DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_ID`, `email`, `password_text`, `username`, `firstname`, `lastname`, `nationality`,
+                     `user_type`, `Interests`, `Gender`, `Age`, `Uni`, `Course`)
+VALUES (4, 'admin@test.com', 'Test123', 'admin', 'test', 'test', 'Cameroon', 'ADMIN', '', '', 0, '0', '0'),
+       (10, 'mike@gmail.com', 'Test123', 'test1', 'test', 'test', 'Ghana', 'REG_USER',
+        'I like football and support Manchester United. I also play the flute and like going to the movies.', 'Male',
+        35, 'Robert Gordon University', 'MSc IT');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+    ADD PRIMARY KEY (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+    MODIFY `user_ID` int(10) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 11;
